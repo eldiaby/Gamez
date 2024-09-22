d1c1238c30fdd69ae06dbbe8275b53c2e5223b61
eldiaby <elgdiab@gmail.com>
2024-09-22 14:29:18 +0300

    Adding footer section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 // Elemant variables
 const newSectionVidoes = document.querySelectorAll("video");
+const currentYear = new Date().getFullYear();
 
 // new games section play when hoved effect
 for (const vidoe of newSectionVidoes) {
@@ -9,3 +10,5 @@ for (const vidoe of newSectionVidoes) {
 for (const vidoe of newSectionVidoes) {
   vidoe.addEventListener("mouseleave", () => vidoe.pause());
 }
+
+document.querySelector(".current-year").textContent = `${currentYear}`;
